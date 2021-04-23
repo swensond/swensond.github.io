6a9e72573307e4d2bc95608bcf12e285c9ee566d
David Swenson <david@swensond.com>
2021-04-23 16:06:19 -0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function App() {
+    return (
+        <p>Building this thing finally</p>
+    );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: [],
+    darkMode: "medoa",
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
